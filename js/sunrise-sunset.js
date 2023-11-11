b722ae15614abb6fcc10658757770134010a733c
AnotherCreator <jreginaldo@protonmail.com>
2023-11-10 23:36:54 -0600

    Add sample JSON output and add noon results

--- js/sunrise-sunset.js
+++ js/sunrise-sunset.js
@@ -12,29 +12,53 @@ const date = new Date();
 function getSunriseSunsetData(formSelection = document.getElementById("dashboard_form_select")){
     //Prevent page refresh
     event.preventDefault();
+    // Default hidden HTML element to later display output data
     const outputDiv = document.getElementById("dashboard_output");
 
     if (formSelection.value === "-1") {
         // Hide output elements when choosing placeholder value
         document.getElementById("sunrise_today").innerHTML = "";
+        document.getElementById("noon_today").innerHTML = "";
         document.getElementById("sunset_today").innerHTML = "";
+
         document.getElementById("sunrise_tmr").innerHTML = "";
+        document.getElementById("noon_tmr").innerHTML = "";
         document.getElementById("sunset_tmr").innerHTML = "";
+
         outputDiv.style.display = "none";
     } else {
+        // Respective URL construction for each day
         const url_today =
             `https://api.sunrisesunset.io/json
             ?lat=${Object.values(LOCATION_COORDINATES)[formSelection.value].latitude}
             &lng=${Object.values(LOCATION_COORDINATES)[formSelection.value].longitude}
             &date=${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
         const url_tomorrow =
-            `https://api.sunrisesunset.io/json?lat=${Object.values(LOCATION_COORDINATES)[formSelection.value].latitude}
+            `https://api.sunrisesunset.io/json
+            ?lat=${Object.values(LOCATION_COORDINATES)[formSelection.value].latitude}
             &lng=${Object.values(LOCATION_COORDINATES)[formSelection.value].longitude}`
 
+        /*{ EXAMPLE RETURNED DATA
+            "results": {
+                "sunrise": "5:49:47 AM",
+                "sunset": "8:23:14 PM",
+                "first_light": "3:57:44 AM",
+                "last_light": "10:15:17 PM",
+                "dawn": "5:18:56 AM",
+                "dusk": "8:54:05 PM",
+                "solar_noon": "1:06:31 PM",
+                "golden_hour": "7:44:26 PM",
+                "day_length": "14:33:26",
+                "timezone": "America/New_York",
+                "utc_offset": -240
+            },
+            "status": "OK"
+        }*/
         fetch(url_today)
             .then(response => response.json())
             .then(data => {
                 document.querySelector('#sunrise_today').innerHTML = data.results.sunrise
+                document.querySelector('#noon_today').innerHTML = data.results.solar_noon
                 document.querySelector('#sunset_today').innerHTML = data.results.sunset
             })
             .catch(error => console.error('Error:', error))
@@ -43,6 +67,7 @@ function getSunriseSunsetData(formSelection = document.getElementById("dashboard
             .then(response => response.json())
             .then(data => {
                 document.querySelector('#sunrise_tmr').innerHTML = data.results.sunrise
+                document.querySelector('#noon_tmr').innerHTML = data.results.solar_noon
                 document.querySelector('#sunset_tmr').innerHTML = data.results.sunset
             })
             .catch(error => console.error('Error:', error))
